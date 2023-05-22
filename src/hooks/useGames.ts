919682a2b68798a26db7fd2a664b94970d26869c
Javlondev <j.abdurakhmanov@octo.uz>
2023-05-22 14:33:57 +0500

    Platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -4,10 +4,17 @@ import {Simulate} from "react-dom/test-utils";
 import {CanceledError} from "axios";
 
 
+export interface Platform {
+    id: number
+    name: string
+    slug: string
+}
+
 export interface Game {
     id: number
     name: string
     background_image: string
+    parent_platforms: { platform: Platform }[]
 }
 
 interface FetchGamesResponse {
